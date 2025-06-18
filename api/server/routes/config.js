0b05fcebb681d051f5916c6538073cd0b633fe83
qlmsv <qulmashev@gmail.com>
2025-06-18 19:26:25 +0700

    тайтл и плагины 2

--- api/server/routes/config.js
+++ api/server/routes/config.js
@@ -51,7 +51,7 @@ router.get('/', async function (req, res) {
 
     /** @type {TStartupConfig} */
     const payload = {
-      appTitle: process.env.APP_TITLE || 'LibreChat',
+      appTitle: process.env.APP_TITLE || 'ВердИИкт',
       socialLogins: req.app.locals.socialLogins ?? defaultSocialLogins,
       discordLoginEnabled: !!process.env.DISCORD_CLIENT_ID && !!process.env.DISCORD_CLIENT_SECRET,
       facebookLoginEnabled:
