3899809745befab8d5eec04dec5dc689defdf8de
qlmsv <qulmashev@gmail.com>
2025-06-18 19:26:09 +0700

    тайтл и плагины

--- packages/data-provider/src/config.ts
+++ packages/data-provider/src/config.ts
@@ -787,7 +787,7 @@ export const defaultEndpoints: EModelEndpoint[] = [
 export const alternateName = {
   [EModelEndpoint.openAI]: 'OpenAI',
   [EModelEndpoint.assistants]: 'Assistants',
-  [EModelEndpoint.agents]: 'Agents',
+  [EModelEndpoint.agents]: 'Ассистенты',
   [EModelEndpoint.azureAssistants]: 'Azure Assistants',
   [EModelEndpoint.azureOpenAI]: 'Azure OpenAI',
   [EModelEndpoint.chatGPTBrowser]: 'ChatGPT',
